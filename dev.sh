9c82d5f78b085d0046f8be19c2aea2a46d95166b
Artsiom Varabei <artsiom.varabei@softnetix.io>
2025-05-02 17:37:39 +0300

    fixes

--- dev.sh
+++ dev.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Start the database
+docker compose up -d postgres
+
+# Wait for the database to be ready
+echo "Waiting for database to be ready..."
+sleep 5
+
+# Start the backend and frontend
 gnome-terminal --tab --title="Backend" --working-directory="$(pwd)/back" -- bash -c "yarn start:dev"
 gnome-terminal --tab --title="Frontend" --working-directory="$(pwd)/front" -- bash -c "yarn dev"
